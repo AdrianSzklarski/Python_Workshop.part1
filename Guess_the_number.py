3a606aa08b3fe855911d4f774dfcb8c6602e56f1
AdrianSzklarski <szklarski.adrian@gmail.com>
2022-04-12 23:28:08 +0200

    adding functionality - interaction between the main file and the module

--- Guess_the_number.py
+++ Guess_the_number.py
@@ -1,13 +1,12 @@
-from files.Gtn_Player import Guess_the_numberP
-from files.Gtn_Computer import Guess_the_numberC
+# from files.Gtn_Player import Guess_the_numberP
+# from files.Gtn_Computer import guess_the_numberC
+from files import Gtn_Computer
 
 # 1a. Jeżeli gra komputer, użytkownik musi zdeklarować liczbę;
 # 1b. W ciągu 10 ruchów użytkownik podaje komputerowi czy wylosował za dużo czy za mało;
 # 2a. Jeżeli gra użytkownik pojawia się komunikat "za dużo", "za mało", "wygrałeś", "podana liczba jest poza zakresem";
 # 3. W każdym przypadku program ma obsłużyć możliwe błędy gry;
 # 4. Gra ma być zaimpelmentowana we Flasku
-
-
 
 if __name__ == '__main__':
     while True:
@@ -16,10 +15,12 @@ if __name__ == '__main__':
             print('Select: You - y, Computer - c: ', end=' ')
             selection = input(' ')
 
-            if 'y' == selection:
-                Guess_the_numberP()
-            if 'c' == selection: # letter c
-                Guess_the_numberC()
+            if 'c' == selection:
+                Gtn_Computer.guess_the_numberC()
+
+            # if 'c' == selection:
+            #     files.Guess_the_numberC()
+
             break
         except NameError:
             print("wrong choice")
--- files/Gtn_Computer.py
+++ files/Gtn_Computer.py
@@ -1,17 +1,16 @@
 import random
 
-while True:
-    try:
-        scope_of_min_numbers = int(input('Specify a range minimum of numbers: '))
-        scope_of_max_numbers = int(input('Specify a range maximum of numbers: '))
-        break
-    except ValueError or NameError:
-        print("You should have given either an int")
+def guess_the_numberC():
+    while True:
+        try:
+            scope_of_min_numbers = int(input('Specify a range minimum of numbers: '))
+            scope_of_max_numbers = int(input('Specify a range maximum of numbers: '))
+            break
+        except ValueError or NameError:
+            print("You should have given either an int")
 
-
-def Guess_the_numberC(scope_of_min_numbers, scope_of_max_numbers):
     draw = random.randint(scope_of_min_numbers, scope_of_max_numbers)
     print('the number drawn was: ', draw)
 
-
-Guess_the_numberC(scope_of_min_numbers, scope_of_max_numbers)
+if __name__ == '__main__':
+    guess_the_numberC()
