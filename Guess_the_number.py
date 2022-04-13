a0edf9bddeafa9553c4691c645215fa1f3093a71
AdrianSzklarski <szklarski.adrian@gmail.com>
2022-04-13 10:41:15 +0200

    added description, Finish

--- Guess_the_number.py
+++ Guess_the_number.py
@@ -1,3 +1,13 @@
+"""     Adrian SZKLARSKI, 04.2022r,
+        A game of guessing numbers:
+
+A game of guessing numbers from 1 to 100 in ten moves
+
+    Returns:      numbers drawn during the game
+    Parameters:   the value of the number in the case of a player
+                  or the result of a draw for a computer
+    Error:        ValueError or NameError
+"""
 from files import Gtn_Computer, Gtn_Player
 
 if __name__ == '__main__':
