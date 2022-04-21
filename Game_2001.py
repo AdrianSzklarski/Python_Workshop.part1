9eb753f3c34fc95ca0c2a5205f5e9a1bc8e97ea6
AdrianSzklarski <szklarski.adrian@gmail.com>
2022-04-21 22:41:36 +0200

    added user selection

--- Game_2001.py
+++ Game_2001.py
@@ -1,11 +1,23 @@
 import random
 
-DICES = ('D3', 'D4', 'D6', 'D8', 'D10', 'D12', 'D20', 'D100')
+DICES = ("D3", "D4", "D6", "D8", "D10", "D12", "D20", "D100")
 Points = 0
 
 
-def player():
-    pass
+def player(dices):
+
+    array = []
+    for i in range(1, 3):
+        while True:
+            play = input('Choose dice from DICES list: ').upper()
+            if play in dices:
+                array.append(play)
+                array.sort()
+                i += 1
+                break
+            else:
+                print('Wrong Value!, Try again')
+    return array
 
 
 def computer():
@@ -13,6 +25,7 @@ def computer():
     array = []
     for i in range(1, 3):
         array.append(random.choice(DICES))
+        array.sort()
         i += 1
     return array
 
@@ -20,8 +33,8 @@ def computer():
 def resume():
     pass
 
-
-player()
+chosse_play = player(DICES)
+print(chosse_play)
 chosse_comp = computer()
 print(chosse_comp)
 resume()
