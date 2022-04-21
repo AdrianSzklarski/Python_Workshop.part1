51ef155373964850e9ac01896f2abc694f67e50f
AdrianSzklarski <szklarski.adrian@gmail.com>
2022-04-21 11:26:01 +0200

    Selection of dice by computer

--- Game_2001.py
+++ Game_2001.py
@@ -1,1 +1,27 @@
-# Standby
+import random
+
+DICES = ('D3', 'D4', 'D6', 'D8', 'D10', 'D12', 'D20', 'D100')
+Points = 0
+
+
+def player():
+    pass
+
+
+def computer():
+
+    array = []
+    for i in range(1, 3):
+        array.append(random.choice(DICES))
+        i += 1
+    return array
+
+
+def resume():
+    pass
+
+
+player()
+chosse_comp = computer()
+print(chosse_comp)
+resume()
